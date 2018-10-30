1c3317780a65ae08f8745b5cd3e8be414b13d082
patrikholcak <patrik@holcak.com>
2018-10-30 11:48:44 +0100

    Update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,18 +1,21 @@
 # Command Aliasses
 alias git=hub
 alias ll='ls -lGa'
-alias l='git lg'
+alias c='git commit -m'
 alias d='git diff --word-diff'
+alias l='git lg'
+alias p='git pull origin $(git rev-parse --abbrev-ref HEAD)'
+alias pp='git push origin $(git rev-parse --abbrev-ref HEAD)'
 alias s='git status'
-alias p='show_status -p'
-alias pp='git pull origin $(git rev-parse --abbrev-ref HEAD)'
-alias e='code'
-alias a='code . && npm start'
-alias i='react-native run-ios'
+alias u='git undo'
+alias ck='checkout_branch $1'
+alias i='react-native run-ios --simulator "iPhone X"'
 alias q='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'  # lock screen
 alias server='open http://localhost:8000 && python -m SimpleHTTPServer'
-alias cdd='cd ..'
 alias virtual='VBoxHeadless -s virtual'
+alias cdd='cd ..'
+alias e='code'
+alias reload='source ~/.zshrc'
 
 # Folder aliasses
 alias dev='cd ~/dev/'
@@ -20,3 +23,15 @@ alias dev='cd ~/dev/'
 # Alias fuck command
 alias fuck='TF_CMD=$(TF_ALIAS=fuck PYTHONIOENCODING=utf-8 TF_SHELL_ALIASES=$(alias) thefuck $(fc -ln -1 | tail -n 1)) && eval $TF_CMD && print -s $TF_CMD'
 export THEFUCK_REQUIRE_CONFIRMATION='false'
+
+
+checkout_branch() {
+  git checkout $1 2> /dev/null
+
+  if [ $? -eq 0 ]; then
+    echo "Switched to an existing branch '$1'"
+  else
+    echo "Switched to a new branch '$1', branched from '$(git rev-parse --abbrev-ref HEAD)'"
+    git checkout -b $1 2> /dev/null
+  fi
+}

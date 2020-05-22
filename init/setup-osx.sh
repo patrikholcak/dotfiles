80f2cc3b089ea3fe42f0adb542bc16e2eceff0f7
patrikholcak <patrik@holcak.com>
2020-05-22 18:26:49 +0200

    update configs

--- init/setup-osx.sh
+++ init/setup-osx.sh
@@ -46,45 +46,40 @@ install_package "Installing hub" brew install hub
 install_package "Installing yarn" brew install yarn
 install_package "Installing MAS" brew install mas
 
-install_package "Installing flow" npm install flow-bin -g
-
 install_package "Homebrew Cask" brew tap caskroom/cask
 install_package "Homebrew Cask" brew tap caskroom/versions
 
 # Install required packages
-install_package "Installing app Chrome Canary" brew cask install google-chrome-canary 2> /dev/null
+install_package "Installing app Brave" brew cask install brave-browser 2> /dev/null
 install_package "Installing app VS Code" brew cask install visual-studio-code 2> /dev/null
 install_package "Installing app 1Password" brew cask install 1password-beta 2> /dev/null
-install_package "Installing app Heroku" brew install heroku/brew/heroku 2> /dev/null
 install_package "Installing app Slack" brew cask install slack-beta 2> /dev/null
-install_package "Installing app Docker" brew cask install docker 2> /dev/null
 install_package "Installing app Sketch" brew cask install sketch 2> /dev/null
+install_package "Installing app Figma" brew cask install figma 2> /dev/null
 # install_package "Installing app VirtualBox" brew cask install virtualbox 2> /dev/null
 install_package "Installing app Trailer" brew cask install trailer 2> /dev/null
-install_package "Installing app Caprine" brew cask install caprine 2> /dev/null
 
 install_package "Installing app qlstephen" brew cask install qlstephen 2> /dev/null
 install_package "Installing app quicklook-json" brew cask install quicklook-json 2> /dev/null
 
 install_package "Installing app The Unarchiver" brew cask install the-unarchiver 2> /dev/null
-install_package "Installing app Zoom.us" brew cask install zoomus 2> /dev/null
 
 # Additional editors & browsers
 install_package "Installing app Sublime Text" brew cask install sublime-text 2> /dev/null
-install_package "Installing app Atom" brew cask install atom 2> /dev/null
+install_package "Installing app Chrome Canary" brew cask install google-chrome-canary 2> /dev/null
 install_package "Installing app Chrome" brew cask install google-chrome 2> /dev/null
 install_package "Installing app Firefox" brew cask install firefox 2> /dev/null
 install_package "Installing app Opera" brew cask install opera 2> /dev/null
 
 # Additional software
 install_package "Installing app CleanMyMac" brew cask install cleanmymac3 2> /dev/null
-install_package "Installing app Dropbox" brew cask install dropbox 2> /dev/null
 install_package "Installing app ImageOptim" brew cask install imageoptim 2> /dev/null
 install_package "Installing app ImageAlpha" brew cask install imagealpha 2> /dev/null
 install_package "Installing app Skype" brew cask install skype 2> /dev/null
 
 install_package "Installing app Giphy Capture" mas install 668208984
 install_package "Installing app Battery Indicator" mas install 1206020918
+install_package "Installing app Dato" mas install 1470584107
 install_package "Installing app Xcode" mas install 497799835
 
 install_package "Cleanup" brew cleanup

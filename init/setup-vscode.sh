3247b373e6a556469f2037e0e45cc1a94bd874cf
Patrik Holcak <patrik@holcak.com>
2021-02-02 15:36:12 +0100

    fix extensions install

--- init/setup-vscode.sh
+++ init/setup-vscode.sh
@@ -16,12 +16,12 @@ fail() {
   exit
 }
 
-install_package() {
+install_extension() {
   name=$1
   shift
   info "$name"
   command=$@
-  $command > /dev/null 2>&1
+  /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/bin/code --install-extension $command > /dev/null 2>&1
   out=$?
   if [ $out -eq 0 ];then
     success "$name"
@@ -36,16 +36,16 @@ cp -r configs/Code ~/Library/Application\ Support/
 
 printf "[ \033[00;33mInstalling Extensions\033[0m ]\n"
 
-install_package "coenraads.bracket-pair-colorizer-2\n" code --install-extension coenraads.bracket-pair-colorizer-2
-install_package "naumovs.color-highlight\n" code --install-extension naumovs.color-highlight
-install_package "dbaeumer.vscode-eslint\n" code --install-extension dbaeumer.vscode-eslint
-install_package "kumar-harsh.graphql-for-vscode\n" code --install-extension kumar-harsh.graphql-for-vscode
-install_package "wix.vscode-import-cost\n" code --install-extension wix.vscode-import-cost
-install_package "orta.vscode-jest\n" code --install-extension orta.vscode-jest
-install_package "silvenon.mdx\n" code --install-extension silvenon.mdx
-install_package "monokai.theme-monokai-pro-vscode\n" code --install-extension monokai.theme-monokai-pro-vscode
-install_package "christian-kohler.npm-intellisense\n" code --install-extension christian-kohler.npm-intellisense
-install_package "christian-kohler.path-intellisense\n" code --install-extension christian-kohler.path-intellisense
-install_package "ricard.postcss\n" code --install-extension ricard.postcss
-install_package "esbenp.prettier-vscode\n" code --install-extension esbenp.prettier-vscode
-install_package "visualstudioexptteam.vscodeintellicode\n" code --install-extension visualstudioexptteam.vscodeintellicode
+install_extension "coenraads.bracket-pair-colorizer-2\n" coenraads.bracket-pair-colorizer-2
+install_extension "naumovs.color-highlight\n" naumovs.color-highlight
+install_extension "dbaeumer.vscode-eslint\n" dbaeumer.vscode-eslint
+install_extension "kumar-harsh.graphql-for-vscode\n" kumar-harsh.graphql-for-vscode
+install_extension "wix.vscode-import-cost\n" wix.vscode-import-cost
+install_extension "orta.vscode-jest\n" orta.vscode-jest
+install_extension "silvenon.mdx\n" silvenon.mdx
+install_extension "monokai.theme-monokai-pro-vscode\n" monokai.theme-monokai-pro-vscode
+install_extension "christian-kohler.npm-intellisense\n" christian-kohler.npm-intellisense
+install_extension "christian-kohler.path-intellisense\n" christian-kohler.path-intellisense
+install_extension "ricard.postcss\n" ricard.postcss
+install_extension "esbenp.prettier-vscode\n" esbenp.prettier-vscode
+install_extension "visualstudioexptteam.vscodeintellicode\n" visualstudioexptteam.vscodeintellicode

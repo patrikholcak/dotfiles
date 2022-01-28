41e8ce0c4261ca7b376900f03bc8b411dd2481d9
Patrik Holčák <patrik@holcak.com>
2022-01-28 20:38:54 +0100

    Update

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -9,6 +9,7 @@ alias pp='git push origin $(git rev-parse --abbrev-ref HEAD)'
 alias s='git status'
 alias u='git undo'
 alias ck='checkout_branch $1'
+alias ckm='git checkout main || git checkout master'
 alias i='react-native run-ios --simulator "iPhone X"'
 alias q='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'  # lock screen
 alias server='open http://localhost:8000 && python -m SimpleHTTPServer'
@@ -20,6 +21,7 @@ alias reload='source ~/.zshrc'
 
 # Folder aliasses
 alias dev='cd ~/dev/'
+alias fv='cd ~/dev/finviz/Finviz-Website'
 
 # Alias fuck command
 alias fuck='TF_CMD=$(TF_ALIAS=fuck PYTHONIOENCODING=utf-8 TF_SHELL_ALIASES=$(alias) thefuck $(fc -ln -1 | tail -n 1)) && eval $TF_CMD && print -s $TF_CMD'
